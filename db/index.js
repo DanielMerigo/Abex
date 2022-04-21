6a18a8ce7ec57b0792c20cb1c368d15b31a53b55
Daniel Merigo <87982882+DanielMerigo@users.noreply.github.com>
2022-04-21 11:51:32 -0300

    Delete connection with database

--- db/index.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("pg"); //Criando uma variavel para conectar com o pg
-const client = new Client({        //Dando outro nome para a variavel 
- connectionString: "postgres://postgres:biscoito2012@localhost:5432/Abex"        //aqui conectaremos com o postgreSQL e com a database
-});
-client.connect(); //Usado para conectar o cliente
-                
-module.exports = client; //Usado para extrair a conexão para outros arquivos de codigos
-
